9ef2b29d6d2626b6ad78cee7d66e4881619cd9e9
Jin Nakano <jin.nakano.canada@gmail.com>
2024-10-04 16:26:33 -0700

    finished

--- dist/ex05.js
+++ dist/ex05.js
@@ -1,12 +1,11 @@
+"use strict";
 // Create a function called sumAllNumbers.
 // This function should accept any number of numbers using a rest parameter.
 // It should return the sum of all the numbers passed to it.
-
-function sumAllNumbers(...numbers: number[]): number {
-  const sum = numbers.reduce((acc, curr) => {
-    return acc + curr;
-  });
-  return sum;
+function sumAllNumbers(...numbers) {
+    const sum = numbers.reduce((acc, curr) => {
+        return acc + curr;
+    });
+    return sum;
 }
-
 console.log(sumAllNumbers(1, 2, 3, 4, 5)); // Expected output: 15
